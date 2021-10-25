5d84af7d23f1c2088a7f7b17f90ae8a027d16e13
AlexGaillard <alex@radish.health>
2021-10-25 19:35:29 -0400

    Completed enhancer tutorial

--- index.js
+++ index.js
@@ -5,44 +5,20 @@ const {
   bindActionCreators
 } = require('./node_modules/redux/dist/redux.js');
 
-/**
- * This is a reducer - a function that takes a current state value and an
- * action object describing "what happened", and returns a new state value.
- * A reducer's function signature is: (state, action) => newState
- *
- * The Redux state should contain only plain JS objects, arrays, and primitives.
- * The root state value is usually an object.  It's important that you should
- * not mutate the state object, but return a new object if the state changes.
- *
- * You can use any conditional logic you want in a reducer. In this example,
- * we use a switch statement, but it's not required.
- */
- function counterReducer(state = { value: 0 }, action) {
-  switch (action.type) {
-    case "counter/incremented":
-      return { value: state.value + 1 };
-    case "counter/decremented":
-      return { value: state.value - 1 };
-    default:
-      return state;
-  }
-}
+const reducer = (state = {count: 1}) => state;
 
-// Create a Redux store holding the state of your app.
-// Its API is { subscribe, dispatch, getState }.
-let store = createStore(counterReducer);
+const logEnhancer = (createStore) => (reducer, initialState, enhancer) => {
+  const loggedEnhancer = (state, action) => {
+    console.log(state, action)
+    const newState = reducer(state, action);
+    console.log(newState, action)
 
-// You can use subscribe() to update the UI in response to state changes.
-// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
-// There may be additional use cases where it's helpful to subscribe as well.
+    return newState;
+  };
 
-store.subscribe(() => console.log(store.getState()));
+  return createStore(loggedEnhancer, initialState, enhancer);
+};
 
-// The only way to mutate the internal state is to dispatch an action.
-// The actions can be serialized, logged or stored and later replayed.
-store.dispatch({ type: "counter/incremented" });
-// {value: 1}
-store.dispatch({ type: "counter/incremented" });
-// {value: 2}
-store.dispatch({ type: "counter/decremented" });
-// {value: 1}
+const store = createStore(reducer, logEnhancer);
+
+store.dispatch({type: 'Hello'});
